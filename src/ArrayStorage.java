e83c213a762be7c3adcb49144bd126693d25026f
RomanKovalenko61 <astek14@mail.ru>
2019-02-17 18:33:41 +0300

    Добавлены исправления в проект по замечаниям (файл rewiew.txt)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,27 +6,23 @@ public class ArrayStorage {
     static int count = 0;
 
     void clear() {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < count; i++) {
             storage[i] = null;
         }
         count = 0;
     }
 
     void save(Resume r) {
-        boolean check = true;
-        String presentName = r.uuid;
-        String checkName;
-            for (int i = 0; i < count; i++) {
-                checkName = storage[i].uuid;
-                if (presentName.equals(checkName)) {
-                    check = false;
+        boolean isObjectInArray = false;
+        for (int i = 0; i < count; i++) {
+                if (r.uuid.equals(storage[i].uuid)) {
+                    isObjectInArray = true;
                 }
-            }
-        if (check) {
+        }
+        if (!isObjectInArray) {
             storage[count] = r;
             count++;
         }
-
     }
 
     Resume get(String uuid) {
@@ -46,11 +42,13 @@ public class ArrayStorage {
                 break;
             }
         }
-        for (int i = cursor; i < size() - 1; i++) {
-            storage[i] = storage[i + 1];
+        if (cursor > -1) {
+            for (int i = cursor; i < count - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[count - 1] = null;
+            count--;
         }
-        count--;
-
     }
 
     /**
@@ -58,7 +56,7 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] storageWithoutNull = new Resume[count];
-        for (int i = 0; i < size(); i++ ) {
+        for (int i = 0; i < count; i++ ) {
             storageWithoutNull[i] = storage[i];
         }
         return storageWithoutNull;
