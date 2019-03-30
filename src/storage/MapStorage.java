5ee8abd3d07cd7e03ffc77313186fc683b6550cb
RomanKovalenko61 <astek14@mail.ru>
2019-03-30 18:06:43 +0300

    HW5: implementation MapStorage

--- src/storage/MapStorage.java
+++ src/storage/MapStorage.java
@@ -1,4 +1,60 @@
 package storage;
 
-public class MapStorage {
+import model.Resume;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+
+    protected static final Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return storage.get(new Resume(uuid));
+    }
+
+    @Override
+    protected boolean resumeIsExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void saveToStorage(Object searchKey, Resume resume) {
+        storage.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected void deleteFromStorage(Object searchKey) {
+        storage.remove(searchKey);
+    }
+
+    @Override
+    protected void updateToStorage(Object searchKey, Resume resume) {
+        storage.put(((Resume) searchKey).getUuid(), resume);
+    }
+
+    @Override
+    protected Resume getFromStorage(Object searchKey) {
+        return storage.get(searchKey);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        Collection resumes = storage.values();
+        Resume[] allResumes = new Resume[storage.size()];
+        resumes.toArray(allResumes);
+        return allResumes;
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
 }
