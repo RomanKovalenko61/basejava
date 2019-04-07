c65b05bd0f689bf8272b35d8c4a730a11fb4dca6
RomanKovalenko61 <astek14@mail.ru>
2019-04-07 17:10:11 +0300

    HW6: add field "fullName", constructor, getter/setter, rewriting methods equals(), hashcode(), toString()

--- src/model/Resume.java
+++ src/model/Resume.java
@@ -8,12 +8,27 @@ public class Resume implements Comparable<Resume> {
     // Unique identifier
     private final String uuid;
 
+    private String fullName;
+
     public Resume(String uuid) {
         this.uuid = uuid;
     }
 
+    public Resume(String uuid, String fullName) {
+        this(uuid);
+        this.fullName = fullName;
+    }
+
     public String getUuid() {
         return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullname) {
+        this.fullName = fullname;
     }
 
     @Override
@@ -23,17 +38,17 @@ public class Resume implements Comparable<Resume> {
 
         Resume resume = (Resume) o;
 
-        return uuid.equals(resume.uuid);
+        return uuid.equals(resume.uuid) && fullName.equals(resume.fullName);
     }
 
     @Override
     public int hashCode() {
-        return uuid.hashCode();
+        return uuid.hashCode() + fullName.hashCode();
     }
 
     @Override
     public String toString() {
-        return uuid;
+        return "uuid = " + uuid + " fullname = " + fullName;
     }
 
     @Override
