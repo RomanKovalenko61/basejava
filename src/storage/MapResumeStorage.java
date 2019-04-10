12bd3ea96a72b1aefdfe84ac059f3e9a8dc07b05
RomanKovalenko61 <astek14@mail.ru>
2019-04-10 21:52:39 +0300

    HW06: add MapResumeStorage

---
+++ src/storage/MapResumeStorage.java
@@ -0,0 +1,57 @@
+package storage;
+
+import model.Resume;
+
+import java.util.*;
+
+public class MapResumeStorage extends AbstractStorage {
+
+    protected static final Map<Resume, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return storage.get(new Resume(uuid));
+    }
+
+    @Override
+    protected boolean resumeIsExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void saveToStorage(Object searchKey, Resume resume) {
+        storage.put(resume, resume);
+    }
+
+    @Override
+    protected void deleteFromStorage(Object searchKey) {
+        storage.remove((Resume) searchKey);
+    }
+
+    @Override
+    protected void updateToStorage(Object searchKey, Resume resume) {
+        storage.put(resume, resume);
+    }
+
+    @Override
+    protected Resume getFromStorage(Object searchKey) {
+        return storage.get((Resume) searchKey);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        List<Resume> sortedList = new ArrayList<>(storage.values());
+        Collections.sort(sortedList);
+        return sortedList;
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
