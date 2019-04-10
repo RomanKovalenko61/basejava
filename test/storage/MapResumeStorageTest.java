2d7016d78bda66f5055a3373430db783b03d9ae7
RomanKovalenko61 <astek14@mail.ru>
2019-04-10 21:53:52 +0300

    HW06: add MapResumeStorageTest

---
+++ test/storage/MapResumeStorageTest.java
@@ -0,0 +1,10 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+public class MapResumeStorageTest extends AbstractArrayStorageTest {
+
+    protected MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
