a45ce9285476d17843c54f14a26a6b4c3ceb340d
RomanKovalenko61 <astek14@mail.ru>
2019-04-17 21:41:08 +0300

    HW06: refactoring

--- test/storage/AllTestStorage.java
+++ test/storage/AllTestStorage.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class,
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
         SortedArrayStorageTest.class,
         ListStorageTest.class,
         MapStorageTest.class,
