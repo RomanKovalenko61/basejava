188031ee120f6fa0b84fda56e77d401640f8dfd7
RomanKovalenko61 <astek14@mail.ru>
2019-04-21 14:59:45 +0300

    HW06: add private class Comparator<Resume>

--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -3,14 +3,22 @@ package storage;
 import model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected Integer getSearchKey(String uuid) {
-        Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        Resume searchKey = new Resume(uuid, "");
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
 
     @Override
     protected void saveToArray(int index, Resume resume) {
